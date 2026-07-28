5ee92b79b3950928907516011e8ffca7f58499f5
Lonelyhackertechs <mkabz1589@gmail.com>
2026-07-28 12:53:21 +0300

    Fix Vite CSS entry for production

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,11 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-
-                'resources/css/tailwind.css',
+                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
-
 
         vue({
             template: {
@@ -23,6 +21,7 @@ export default defineConfig({
             },
         }),
     ],
+
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
