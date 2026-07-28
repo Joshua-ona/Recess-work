abc07058d1de987ee4f2b00fe6a89fe772c9c91c
Lonelyhackertechs <mkabz1589@gmail.com>
2026-07-28 14:14:37 +0300

    Start recommendation engine with Laravel

--- app/recommendation_engine/deployment/api.py
+++ app/recommendation_engine/deployment/api.py
@@ -145,16 +145,8 @@ def home():
 
 if __name__ == "__main__":
 
-    port = int(
-        os.environ.get(
-            "PORT",
-            5001
-        )
-    )
-
-
     app.run(
         host="0.0.0.0",
-        port=port,
+        port=5001,
         debug=False
     )
--- docker/start.sh
+++ docker/start.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
+
 php artisan migrate --force || true
+
 php artisan storage:link || true
+
 php artisan optimize:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
+
+echo "Starting Recommendation Engine..."
+
+cd recommendation_engine/deployment
+
+python3 api.py &
+
+
+echo "Starting Laravel..."
+
+cd /var/www/html
 
 php artisan serve --host=0.0.0.0 --port=$PORT
